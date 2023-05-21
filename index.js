e7db1f34de25626944703502ffc278851ac98cfc
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-05-20 22:03:41 -0700

    add triangle, square, circle and shaps classes

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const triangle=require('./lib/Triangle');
+const circle = require('./lib/square');
+const square = require('./lib/square');
+const fs = require('fs');
